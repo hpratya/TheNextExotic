b4cdf9bc5f411c487e65edbaba2a80c0c483f102
Pratya <pratya4xd@gmail.com>
2022-01-11 21:41:33 +0700

    authen update

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:thenextexoticeflutter/utillit/my_constant.dart';
+import 'package:thenextexoticeflutter/widget/show_image.dart';
+import 'package:thenextexoticeflutter/widget/show_title.dart';
 
 class Authen extends StatefulWidget {
   const Authen({Key? key}) : super(key: key);
@@ -10,10 +13,115 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  bool statusRedEye = true;
+
   @override
   Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SafeArea(child: Text('This is Authen Page.'),),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            BuildImage(size),
+            BuildAppName(),
+            BuildUser(size),
+            BuildPassword(size),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row BuildPassword(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.8,
+          child: TextFormField(
+            obscureText: statusRedEye,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(horizontal: size * 0.1),
+              prefixIcon: Icon(
+                Icons.lock_outline,
+                color: MyConstant.darkColor,
+              ),
+              suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      statusRedEye = !statusRedEye;
+                    });
+                  },
+                  icon: Icon(
+                    statusRedEye
+                        ? Icons.remove_red_eye_sharp
+                        : Icons.remove_red_eye_outlined,
+                    color: MyConstant.darkColor,
+                  )),
+              hintText: 'Password',
+              hintStyle: TextStyle(color: MyConstant.darkColor),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.darkColor),
+                  borderRadius: BorderRadius.circular(15)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.darkColor),
+                  borderRadius: BorderRadius.circular(15)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row BuildUser(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.8,
+          child: TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(horizontal: size * 0.1),
+              prefixIcon: Icon(
+                Icons.email_outlined,
+                color: MyConstant.darkColor,
+              ),
+              hintText: 'Email Address',
+              hintStyle: TextStyle(color: MyConstant.darkColor),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.darkColor),
+                  borderRadius: BorderRadius.circular(15)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.darkColor),
+                  borderRadius: BorderRadius.circular(15)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row BuildAppName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(title: MyConstant.appName, textStyle: MyConstant().h1Style()),
+      ],
+    );
+  }
+
+  Row BuildImage(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: ShowImage(path: MyConstant.imageLogo),
+        ),
+      ],
     );
   }
 }
--- lib/utillit/my_constant.dart
+++ lib/utillit/my_constant.dart
@@ -9,4 +9,29 @@ class MyConstant {
   static String routCreateAccount = '/createAccount';
   static String routBuyer = '/buyerService';
   static String routSeller = '/sellerService';
+
+  //Image
+  static String imageLogo = 'images/logoRBG.png';
+
+  //Colors
+  static Color primaryColor = Color(0xfffbc02d);
+  static Color darkColor = Color(0xffc49000);
+  static Color lightColor = Color(0xfffff263);
+
+  //Style
+  TextStyle h1Style() => TextStyle(
+        fontSize: 24,
+        color: darkColor,
+        fontWeight: FontWeight.bold,
+      );
+  TextStyle h2Style() => TextStyle(
+        fontSize: 16,
+        color: lightColor,
+        fontWeight: FontWeight.w700,
+      );
+  TextStyle h3Style() => TextStyle(
+        fontSize: 14,
+        color: primaryColor,
+        fontWeight: FontWeight.normal,
+      );
 }
