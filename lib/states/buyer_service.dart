ebae1fab2c089b0467de6e0fb0bb7e6576f8418b
Pratya <pratya4xd@gmail.com>
2022-01-11 18:40:17 +0700

    Create Service Page

---
+++ lib/states/buyer_service.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BuyerServive extends StatefulWidget {
+  const BuyerServive({Key? key}) : super(key: key);
+
+  @override
+  _BuyerServiveState createState() => _BuyerServiveState();
+}
+
+class _BuyerServiveState extends State<BuyerServive> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/utillit/my_constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  //General
+  static String appName = 'The NextExotic';
+
+  //Route
+  static String routAuthen = '/authen';
+  static String routCreateAccount = '/createAccount';
+  static String routBuyer = '/buyerService';
+  static String routSeller = '/sellerService';
+}
